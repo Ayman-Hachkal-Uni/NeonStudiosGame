fbfcfe570ed7f19e686d8144f2f60d006d7fcc28
Ben Fleming <benbfleming@gmail.com>
2024-10-30 14:08:07 +0000

    Started createBuilding method in BuildMaster + Added BuildingEnum

--- UniSim/core/src/main/java/io/github/NeonStudiosGame/BuildMaster/BuildMaster.java
+++ UniSim/core/src/main/java/io/github/NeonStudiosGame/BuildMaster/BuildMaster.java
@@ -1,10 +1,28 @@
 package io.github.NeonStudiosGame.BuildMaster;
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TiledMapTileLayer;
+import io.github.NeonStudiosGame.buildings.*;
 
-import com.badlogic.gdx.scenes.scene2d.ui.Cell;
+import java.util.LinkedList;
+import java.util.List;
+
 
 public class BuildMaster {
+    List<BaseBuilding> buildings = new LinkedList<>();
     public BuildMaster() {
 
+    }
+
+    public void createBuilding(int[] position) {
+        BuildingEnum selection = BuildingEnum.HALLS;
+        BaseBuilding building = switch (selection) {
+            case BASE_BUILDING -> new BaseBuilding(position);
+            case HALLS -> new Halls(position);
+            case LECTURE_THEATRE -> new LectureTheatre(position);
+            case RESTAURANT -> new Restaurant(position);
+            case SPORTS_HALL -> new SportsHall(position);
+        };
+        buildings.add(building);
     }
 
 }
---
+++ UniSim/core/src/main/java/io/github/NeonStudiosGame/buildings/BuildingEnum.java
@@ -0,0 +1,9 @@
+package io.github.NeonStudiosGame.buildings;
+
+public enum BuildingEnum {
+    BASE_BUILDING,
+    HALLS,
+    LECTURE_THEATRE,
+    RESTAURANT,
+    SPORTS_HALL
+}
