5670414ec374666b327b34bd843bf95c48ed9a70
KenzFountain <wlq503@york.ac.uk>
2024-11-11 09:48:48 +0000

    Javadocs added, small fixes to settings

--- UniSim/core/src/main/java/io/github/NeonStudiosGame/AppPreferences.java
+++ UniSim/core/src/main/java/io/github/NeonStudiosGame/AppPreferences.java
@@ -7,23 +7,27 @@ import com.badlogic.gdx.Preferences;
 import com.badlogic.gdx.audio.Music;
 import com.badlogic.gdx.audio.Sound;
 
-
+/**
+ * The app preferences in the settings page are stored here. This class contains getter and setter
+ * methods for the different settings to ensure that settings can be tweaked and are saved upon leaving
+ * the settings screen.
+ */
 public class AppPreferences {
     public static Music music = Gdx.audio.newMusic(Gdx.files.internal("game-music.mp3")); // PLACEHOLDER menu music
 
     private static final String Settings_Music_Vol = "Volume";
     private static final String Settings_Sound_Vol = "SFX Volume?";
-    private static final String Settings_High_Contrast = "High_Contrast?";
-    private static final String Settings_Large_Font = "Large_Font?";
+    //private static final String Settings_High_Contrast = "High_Contrast?";
+    //private static final String Settings_Large_Font = "Large_Font?";
     private static final String Settings_Name = "UniSim_Settings";
     private static final String Settings_Fullscreen = "Fullscreen_Enabled?";
 
     Sound soundTest = Gdx.audio.newSound(Gdx.files.internal("game_pop_sound.wav"));
 
-    /*
-    This class is constructed of a list of getters and setters which are called by the
-    SettingsScreen class through the parent class UniSim. TEMP not fully implemented yet,
-    and needs some more work - sliders reset value when settings is closed.
+    /**
+     * This class is constructed of a list of getters and setters which are called by the
+     * SettingsScreen class through the parent class UniSim. All methods below either get
+     * or set the current state of different settings.
      */
 
     protected Preferences getSettings() {
@@ -47,7 +51,7 @@ public class AppPreferences {
     public void setSoundVolume(float volume) {
         getSettings().putFloat(Settings_Sound_Vol, volume);
         if(Gdx.input.isButtonPressed(Input.Buttons.LEFT)) {
-            soundTest.play();
+            soundTest.play(volume);
         }
         getSettings().flush();
     }
@@ -75,6 +79,12 @@ public class AppPreferences {
     public boolean isFullscreenEnabled() {
         return getSettings().getBoolean(Settings_Fullscreen, true);
     }
+
+    /**
+     * Slightly more complicated setter, sets the game to fullscreen if toggled on, or
+     * to windowed mode if toggled off.
+     * @param fullscreenEnabled bool, true if the game window is currently fullscreen
+     */
     public void setFullscreenEnabled(boolean fullscreenEnabled) {
         getSettings().putBoolean(Settings_Fullscreen, fullscreenEnabled);
         if(fullscreenEnabled) {
